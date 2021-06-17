d0c9b6af840762878e44d7531efd82cb0ae3669f
Murilo Barreto <barreto_11almeida@hotmail.com>
2021-06-16 23:34:49 -0300

    gerenciamento dos quizzes

--- lib/challenge/widgets/question_indicator/question_indicator_widget.dart
+++ lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -3,6 +3,11 @@ import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_wid
 import 'package:flutter/material.dart';
 
 class QuestionIndicatorWidget extends StatelessWidget {
+  final int currentPage;
+  final int length;
+  const QuestionIndicatorWidget({Key? key, required this.currentPage, required this.length
+  });
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,13 +17,13 @@ class QuestionIndicatorWidget extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("Questão 04", style: AppTextStyles.body,),
-              Text("de 10", style: AppTextStyles.body,)
+              Text("Questão $currentPage", style: AppTextStyles.body,),
+              Text("de $length", style: AppTextStyles.body,)
             ],
           ),
           SizedBox(height: 16,),
           ProgressIndicatorWidgets(
-            value: 0.7,
+            value: currentPage/ length,
           )
         ],
       )
